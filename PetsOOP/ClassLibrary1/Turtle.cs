ab4e8a599baa44216f9e29c2af72aace9c999a94
darrenr21 <darrenr3018@gmail.com>
2024-01-23 19:58:22 -0500

    First version
    OOP 2024 example

---
+++ PetsOOP/ClassLibrary1/Turtle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Turtle : Pet
+    {
+        public Turtle(string name) : base(name)
+        {
+
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine(Name + " grunts at you.");
+        }
+        public void HideInShell()
+        {
+            Console.WriteLine(Name + " hides in it's shell.");
+        }
+        public void Meet(Turtle newFriend)
+        {
+            Console.WriteLine(Name + " scratches at " + newFriend.Name);
+        }
+    }
+}
